e21888104ffda6fb82c4f1cbb22766ee16a4bc60
Matt Galbraith <MattGal@users.noreply.github.com>
2021-08-13 08:49:52 -0700

    Remove docker installation from old TX2 setup
    Ideally we should remove usage of this script entirely, but for now to unblock users I'm making a fork without docker installation.

--- src/jetson-tx2/setup.sh
+++ src/jetson-tx2/setup.sh
@@ -68,22 +68,6 @@ apt-get install -y \
     curl \
     software-properties-common
 
-if ! [ -x "$(command -v docker)" ]; then
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
-    add-apt-repository \
-    "deb [arch=arm64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-
-    apt-get update
-    apt-get install -y docker.ce
-
-    groupadd docker
-
-    usermod -aG docker $username
-fi
-
 usermod -aG docker $username
 
 # Install the rest of the dependencies
